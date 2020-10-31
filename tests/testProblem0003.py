15c55991663181890882d7b435d76bf6405a51f5
uho <urs.hofmaenner@gmail.com>
2020-11-01 00:00:59 +0100

    refactor Problem0003

--- tests/testProblem0003.py
+++ tests/testProblem0003.py
@@ -1,20 +1,21 @@
 import unittest
-from problems.Problem0003 import prime_factors_of
+from problems.Primes import prime_factors_of
 
 
 class TestProblem0003(unittest.TestCase):
     def test_prime_factors_of_3(self):
         p = prime_factors_of(3)
-        self.assertListEqual([3],p)
+        self.assertListEqual([3], p)
 
     def test_prime_factors_of_6(self):
         p = prime_factors_of(6)
-        self.assertListEqual([2,3],p)
+        self.assertListEqual([2, 3], p)
 
     def test_greatest_prime_factor_of_600851475143(self):
         p = prime_factors_of(600851475143)
         print(p)
         self.assertEqual(6857, p[-1])
+
 
 if __name__ == '__main__':
     unittest.main()
