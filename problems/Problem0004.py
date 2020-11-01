691bffa7c67352e9757068be88592d8b7c17590f
uho <urs.hofmaenner@gmail.com>
2020-11-01 18:14:28 +0100

    fix Problem0004

--- problems/Problem0004.py
+++ problems/Problem0004.py
@@ -1,29 +1,22 @@
-from problems.Primes import prime_factors_of
-
-
-def palindrome_of_3(half):
-    return ((half * 10 + half % 10) * 10 + half // 10 % 10) * 10 + half // 100
-
-
-def build_3_digit_factor(factors, first_factor):
-    while len(factors) > 0 and first_factor * factors[0] < 1000:
-        first_factor *= factors[0]
-        factors = factors[1:len(factors)]
-    return factors, first_factor
+def is_palindrome(number):
+    digits = [x for x in str(number)]
+    if len(digits) % 2 != 0:
+        return False
+    for i in range(len(digits) // 2):
+        if digits[i] != digits[-i - 1]:
+            return False
+    return True
 
 
 def find_greatest_palindrome():
-    half = 999
-    while half > 99:
-        palindrome = palindrome_of_3(half)
-        factors = prime_factors_of(palindrome)
-        if factors[-1] < 1000:
-            factors, first_factor = build_3_digit_factor(factors, factors.pop())
-            factors, second_factor = build_3_digit_factor(factors, 1)
-
-            print(palindrome, prime_factors_of(palindrome), [first_factor, second_factor], factors)
-
-            if len(factors) == 0:
-                return palindrome
-        half -= 1
-    raise ValueError
+    largest_palindrome = 0
+    a = 999
+    while a >= 100:
+        b = a
+        while b >= 100:
+            c = a * b
+            if is_palindrome(c):
+                largest_palindrome = max(largest_palindrome, c)
+            b -= 1
+        a -= 1
+    return largest_palindrome
--- tests/testProblem0004.py
+++ tests/testProblem0004.py
@@ -1,14 +1,19 @@
 import unittest
-from problems.Problem0004 import palindrome_of_3, find_greatest_palindrome
+from problems.Problem0004 import is_palindrome, find_greatest_palindrome
 
 
 class TestProblem0004(unittest.TestCase):
-    def test_palinedrome_of_3(self):
-        self.assertEqual(123321, palindrome_of_3(123))
-        self.assertEqual(100001, palindrome_of_3(100))
+    def test_is_palindrome(self):
+        palindromes = set(map(lambda x: palindrome_of_3(x), range(100, 1000)))
+        for i in range(100000, 1000000):
+            self.assertEqual(is_palindrome(i), i in palindromes)
 
-    def test_factors(self):
+    def test_find_greatest_palindrome(self):
         self.assertEqual(906609, find_greatest_palindrome())
+
+
+def palindrome_of_3(half):
+    return ((half * 10 + half % 10) * 10 + half // 10 % 10) * 10 + half // 100
 
 
 if __name__ == '__main__':
